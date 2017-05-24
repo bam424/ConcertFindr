f2dc87d6ebd558d865a74127ad656f481fdc31f0
DereK Han <derekhan206@gmail.com>
2017-05-24 12:24:01 -0700

    Adds loading wheel feedback on both city input and date range picker screens, adds uidatepicker for input in the two date fields, adds working API request for metroid and events

--- ConcertFindr/ConcertFindr/CityInputViewController.swift
+++ ConcertFindr/ConcertFindr/CityInputViewController.swift
@@ -7,14 +7,22 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class CityInputViewController: UIViewController {
 
+    var metroID: String = ""
+    private var SongKickAPIKey: String = "HqtbfXIKRDQWYRLi"
     
+    @IBOutlet weak var loadingWheel: UIActivityIndicatorView!
     @IBOutlet weak var cityNameInputField: UITextField!
-    private var dateRangeIdentifier: String = "DateRangeInputViewController"
+    private var dateRangeIdentifier: String = "DateRangeInputSegue"
     
     override func viewDidLoad() {
+        loadingWheel.hidesWhenStopped = true
+        loadingWheel.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.gray
+        loadingWheel.center = view.center
         super.viewDidLoad()
     }
 
@@ -23,13 +31,31 @@ class CityInputViewController: UIViewController {
     }
     
     @IBAction func addDateRangeButtonClick(_ sender: UIButton) {
-        self.performSegue(withIdentifier: dateRangeIdentifier, sender: self)
+        loadingWheel.startAnimating()
+        Alamofire.request("http://api.songkick.com/api/3.0/search/locations.json?apikey=\(SongKickAPIKey)&query=\(self.cityNameInputField.text!)&per_page=1")
+            .validate(statusCode: 200..<300)
+            .validate(contentType:["application/json"])
+            .responseJSON { response in
+                switch response.result {
+                case .success:
+                    if let retrievedData = response.data {
+                        let json = JSON(data: retrievedData)
+                        self.metroID = String(describing: json["resultsPage"]["results"]["location"][0]["metroArea"]["id"])
+                        self.performSegue(withIdentifier: self.dateRangeIdentifier, sender: self)
+                    }
+                    self.loadingWheel.stopAnimating()
+                case .failure(let error):
+                    print("\(error)")
+                    print("failed")
+                    self.loadingWheel.stopAnimating()
+                }
+        };
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == dateRangeIdentifier {
             let viewController = segue.destination as! DatePickerViewController
-            viewController.cityName = self.cityNameInputField.text!
+            viewController.metroID = self.metroID
         }
     }
     
