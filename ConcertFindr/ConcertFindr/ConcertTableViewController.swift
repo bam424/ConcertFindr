2013f032582b603f1a2e1b244704cb5c796d5c9a
brandon <brandon42496@yahoo.com>
2017-05-30 16:28:39 -0700

    successfully updated the cells to include the artists

--- ConcertFindr/ConcertFindr/ConcertTableViewController.swift
+++ ConcertFindr/ConcertFindr/ConcertTableViewController.swift
@@ -37,17 +37,25 @@ class ConcertTableViewController: UITableViewController {
 
     // MARK: - Table view data source
 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return concerts.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ConcertCell", for: indexPath)
+        let concert = concerts[indexPath.row]
+        cell.textLabel?.text = concert.artist[0]
+        cell.detailTextLabel?.text = concert.venueName
+        return cell
+    }
+
+    /*
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 0
     }
 
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
-
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
 
