6150a1f3a2881eafb25bc9c516ff677e3a809890
DereK Han <derekhan206@gmail.com>
2017-06-05 09:42:35 -0700

    Adds data transfer between segue from mapview to detailview, still missing info button

--- ConcertFindr/ConcertFindr/VCMapView.swift
+++ ConcertFindr/ConcertFindr/VCMapView.swift
@@ -9,11 +9,12 @@
 import Foundation
 import MapKit
 
+var selectedAnnotation: ConcertAnnotation!
+
 extension MapViewController: MKMapViewDelegate {
     
-    // 1
-    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
-        if let annotation = annotation as? ConcertPin {
+    private func mapView(_ mapView: MKMapView, viewFor annotation: ConcertAnnotation) -> MKAnnotationView? {
+        if let annotation = annotation as? ConcertAnnotation {
             let identifier = "pin"
             var view: MKPinAnnotationView
             if let dequeuedView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
@@ -24,8 +25,9 @@ extension MapViewController: MKMapViewDelegate {
                 // 3
                 view = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
                 view.canShowCallout = true
-                view.calloutOffset = CGPoint(x: -5, y: 5)
-                view.rightCalloutAccessoryView = UIButton(type: .detailDisclosure) as UIView
+                view.isEnabled = true
+//                view.calloutOffset = CGPoint(x: -5, y: 5)
+                view.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
             }
             return view
         }
@@ -34,15 +36,19 @@ extension MapViewController: MKMapViewDelegate {
     
     func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
         if control == view.rightCalloutAccessoryView {
-            print("performing segue")
+            selectedAnnotation = view.annotation as! ConcertAnnotation
             performSegue(withIdentifier: "MapToDetails", sender: view)
         }
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "MapToDetails" {
-            var detailsPage = segue.destination as! ConcertDetailsViewController
-            
+            let detailsPage = segue.destination as! ConcertDetailsViewController
+            detailsPage.concertTitle = selectedAnnotation.title
+            detailsPage.ageRestriction = selectedAnnotation.concert?.ageRestriction
+            detailsPage.startTime = selectedAnnotation.concert?.startTime
+            detailsPage.eventDate = selectedAnnotation.concert?.eventDate
+            detailsPage.ticketURL = selectedAnnotation.concert?.ticketsURL
         }
     }
 }
