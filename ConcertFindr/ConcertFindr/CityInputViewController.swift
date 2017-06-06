1ebd0f388f11690333acd81f8fc42b876a0030b3
Patricia Au <patau@uw.edu>
2017-06-06 00:35:01 -1000

    Use user location if specified

--- ConcertFindr/ConcertFindr/CityInputViewController.swift
+++ ConcertFindr/ConcertFindr/CityInputViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 import Alamofire
 import SwiftyJSON
+import CoreLocation
+import MapKit
 
 // Put this piece of code anywhere you like
 extension UIViewController {
@@ -23,16 +25,18 @@ extension UIViewController {
     }
 }
 
-class CityInputViewController: UIViewController, UITextFieldDelegate {
+class CityInputViewController: UIViewController, UITextFieldDelegate,CLLocationManagerDelegate {
 
     var metroID: String = ""
+    var locationLat: Double = 0
+    var locationLong: Double = 0
     private var SongKickAPIKey: String = "HqtbfXIKRDQWYRLi"
     
     @IBOutlet weak var loadingWheel: UIActivityIndicatorView!
     @IBOutlet weak var cityNameInputField: UITextField!
     @IBOutlet weak var noCityResultImage: UIImageView!
     
-    
+    let locationManager = CLLocationManager()
     private var dateRangeIdentifier: String = "DateRangeInputSegue"
     
     override func viewDidLoad() {
@@ -46,6 +50,29 @@ class CityInputViewController: UIViewController, UITextFieldDelegate {
         swipeLeft.direction = UISwipeGestureRecognizerDirection.left
         self.view.addGestureRecognizer(swipeLeft)
         self.cityNameInputField.delegate = self
+        
+        self.locationManager.requestAlwaysAuthorization()
+        self.locationManager.requestWhenInUseAuthorization()
+
+    }
+
+    @IBAction func getCurrentLocation(_ sender: Any) {
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self as CLLocationManagerDelegate
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.startUpdatingLocation()
+        }
+
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations[0]
+        if self.locationLat == 0 && self.locationLong == 0 {
+            self.locationLat = (locationManager.location?.coordinate.latitude)!
+            self.locationLong = (locationManager.location?.coordinate.longitude)!
+            print("Set to \(locationLat) & \(locationLong)")
+            cityNameInputField.text = "\(locationLat),\(locationLong)"
+        }
     }
 
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
@@ -72,7 +99,15 @@ class CityInputViewController: UIViewController, UITextFieldDelegate {
     
     @IBAction func addDateRangeButtonClick(_ sender: Any) {
         loadingWheel.startAnimating()
-        Alamofire.request("http://api.songkick.com/api/3.0/search/locations.json?apikey=\(SongKickAPIKey)&query=\(self.cityNameInputField.text!)&per_page=1")
+        
+        var searchURL: URL = URL(string: "http://api.songkick.com/api/3.0/search/locations.json?apikey=\(SongKickAPIKey)&query=\(self.cityNameInputField.text!)&per_page=1")!
+        
+        let numberCharacters = NSCharacterSet.decimalDigits
+        if cityNameInputField.text!.rangeOfCharacter(from: numberCharacters) != nil { //Must be a coord - lat & long
+            print("Is coord")
+            searchURL = URL(string: "http://api.songkick.com/api/3.0/search/locations.json?location=geo:\(cityNameInputField.text!)&apikey=\(SongKickAPIKey)&per_page=1")!
+        }
+        Alamofire.request(searchURL)
             .validate(statusCode: 200..<300)
             .validate(contentType:["application/json"])
             .responseJSON { response in
